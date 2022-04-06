5f8429d2dab2911b79517aed68691580b86ed16f
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-04-06 16:55:47 +0530

    updated readme file for 8th step

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
+      <h1>Increment/Decrement Counter in ReduxToolkit</h1>
+     <Counter/>
     </div>
   );
 }
